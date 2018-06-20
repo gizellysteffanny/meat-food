89a10e556cfb4578cdad22893938d814dee3abfd
Gizelly Steffanny <gizelly.silva@tap4mobile.com.br>
2018-06-20 17:10:37 -0400

    Consumindo serviço  review e formatando dados com os pipes async e date.

--- src/app/restaurants/restaurant.service.ts
+++ src/app/restaurants/restaurant.service.ts
@@ -17,7 +17,7 @@ export class RestaurantService {
   restaurants(): Observable<Restaurant[]> {
     return this.http.get(`${MEAT_API}/restaurants`)
       .map(response => response.json())
-      .catch(ErrorHandler.handlerError)
+      .catch(ErrorHandler.handlerError);
       /* 
       É necessário fazer o map porque nessa chamada vai retornar um objeto chamado Response, e o tipo Response tem outros dados, tem o status code, tem os proprios dados que esperamos receber, as possiveis mensagens de erros.
       Mas como estamos apenas interessado nos dados json.
@@ -28,7 +28,13 @@ export class RestaurantService {
   restaurantById(id: string): Observable<Restaurant> {
     return this.http.get(`${MEAT_API}/restaurants/${id}`)
       .map(response => response.json())
-      .catch(ErrorHandler.handlerError)
+      .catch(ErrorHandler.handlerError);
+  }
+
+  reviewsOfRestaurant(id: string): Observable<any> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handlerError);
   }
 
 }
