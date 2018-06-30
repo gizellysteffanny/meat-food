afea0c6acbdc319f43fa55273e4e1735f5b05867
Gizelly Steffanny <gizelly.silva@tap4mobile.com.br>
2018-06-30 00:35:45 -0400

    Validação de busca com itens não encontrados

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -2,9 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/from';
 
 import { Restaurant } from './restaurant/restaurant.model';
 import { RestaurantService } from './restaurant.service';
@@ -50,7 +53,8 @@ export class RestaurantsComponent implements OnInit {
       .debounceTime(500)
       .distinctUntilChanged()
       .switchMap(searchTerm =>
-        this.restaurantService.restaurants(searchTerm))
+        this.restaurantService.restaurants(searchTerm)
+        .catch(error => Observable.from([])))
       .subscribe(restaurants => this.restaurants = restaurants);
 
     this.restaurantService.restaurants()
