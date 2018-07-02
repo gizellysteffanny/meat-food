3fe6111a38fa4fa7f81dabd8451150ff36d2b539
Gizelly Steffanny <gizelly.silva@tap4mobile.com.br>
2018-07-01 21:39:36 -0400

    Criando o serviço de login

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,12 +5,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InputComponent } from './input/input.component';
 import { RadioComponent } from './radio/radio.component';
 import { RatingComponent } from './rating/rating.component';
+import { SnackbarComponent } from './messages/snackbar/snackbar.component';
 
 import { NotificationService } from './messages/notification.service';
 import { RestaurantService } from '../restaurants/restaurant.service';
 import { ShoppingCartService } from '../restaurant-detail/shopping-cart/shopping-cart.service';
 import { OrderService } from '../order/order.service';
-import { SnackbarComponent } from './messages/snackbar/snackbar.component';
+import { LoginService } from '../security/login/login.service';
 
 @NgModule({
     declarations: [
@@ -35,7 +36,7 @@ export class SharedModule {
     static forRoot(): ModuleWithProviders {
         return {
             ngModule: SharedModule,
-            providers: [ShoppingCartService, RestaurantService, OrderService, NotificationService]
+            providers: [ShoppingCartService, RestaurantService, OrderService, NotificationService, LoginService]
         };
     }
 }
